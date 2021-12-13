81a0a8249431c873e01bbb81a453804ab6ee44d8
jmch-git <94821167+jmch-git@users.noreply.github.com>
2021-12-13 14:43:02 -0800

    Created a newArray to bring in the data from the API

--- js/script.js
+++ js/script.js
@@ -1,60 +1,21 @@
-
-
-// console.log ($.ajax("https://hp-api.herokuapp.com/api/characters"));
-
 // //VARIABLES
-//     const newArray = $.ajax("https://hp-api.herokuapp.com/api/characters".then; 
-
-//     //Create a new arrry with the data from the API
-//     const characterArrayName = URL.name[0];
-//     console.log(characterArrayName);
-
-
-//Variables
+let newArray = [];
 const URL = 'https://hp-api.herokuapp.com/api/characters';
 
-//Element references
-// const $title = $('#title');
-// const $year = $('#year');
-// const $rated = $('#rated')
+//Element references)
 const $form = $("form");
 const $input =$("input[type='text']");
 
 //Event Listeners
-$form.on("submit", handleGetData);
 
+//
+init();
 
 //Functions
-
-function handleGetData(evt){
-    evt.preventDefault();
-    
-    const userInput = $input.val()
-
-    //Check to make sure that there is something that was inputted before the Find Film button was clicked
-  // ********  if(!userInput) return; //get outta here
-    
-    $input.val("");
-
+//Create a new arrry with the data from the API
+function init(){
     $.ajax(URL).then(function(data){
-
-        console.log("In the ajax URL function, right before calling render")
-        render(data);
-    }, function(error){
-        console.log("Something Went Wrong")
-        console.log(error);
-})
+        newArray = data;
+        console.log(data);
+    })
 }
-
-function render(charData) {
-    console.log("In the render function")
-    $('main').html(`
-        <h3>Name: ${charData.name}</h3>
-        <p>Ancestry: ${charData.ancestry}</p>
-        <p>House: ${charData.house}</p>
-        <p>Patronus: ${charData.patronus}</p>
-        <p>Living: ${charData.alive}</p>
-        <img src="${charData.image}" alt="${charData.name}">
-    `);
-}
-
