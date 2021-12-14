cace76a903f403695c587a32611003edff9fa0e6
jmch-git <94821167+jmch-git@users.noreply.github.com>
2021-12-14 07:43:06 -0800

    Created the event input that retrieves the input and calls the handleData function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // //VARIABLES
-let newArray = [];
+//Geting API
 const URL = 'https://hp-api.herokuapp.com/api/characters';
+//Creating a newArray to put the API in
+let newArray = [];
+//Creating variables to put the key/values in
 let selectedName = "";  
 let selectedAncestry = "";
 let selectedPatronus = "";
@@ -9,60 +12,61 @@ let selectedHouse = "";
 let selectedSpecies = "";
 let selectedWizard = "";
 
+
 //Element references)
+//Creating jQuery variables for the form input value
 const $form = $("form");
 const $input =$("input[type='text']");
 
 
-// //Event Listeners
-// $form.on("submit", handleGetData);
+//Event Listeners
+$form.on("submit", handleGetData);
 
 //
 init();
 
-
 //Functions
-
-// function handleGetData(evt){
-//     evt.preventDefault();
-    
-//     const userInput = $input.val()
-
-//     console.log(userInput);
-
-//     //Check to make sure that there is something that was inputted before the Find Film button was clicked
-//     if(!userInput) return; //get outta here
-    
-//     $input.val("");
-
-    
 //Create a new arrry with the data from the API
 function init(){
     $.ajax(URL).then(function(data){
         newArray = data;
         console.log(data);
-
-        function charName(matchInput) {
-            return matchInput.name === "Harry Potter";
-          }
-          
-          selectedName = newArray.find(charName).name;  
-          selectedSpecies = newArray.find(charName).species;
-          selectedAncestry = newArray.find(charName).ancestry; 
-          selectedWizard = newArray.find(charName).wizard; 
-          selectedPatronus = newArray.find(charName).patronus;
-          selectedHouse = newArray.find(charName).house;
-          selectedImage = newArray.find(charName).image;
-          
-          $('main').html(`
-          <h3>Name: ${selectedName}</h3>
-          <p>Species: ${selectedSpecies}</p>
-          <p>Ancestry: ${selectedAncestry}</p>
-          <p>Wizard: ${selectedWizard}</p>
-          <p>Patronus: ${selectedPatronus}</p>
-          <p>House: ${selectedHouse}</p>          
-          <img src="${selectedImage}" alt="${selectedName}">
-      `);
     })
 }
-//}
+
+function handleGetData(evt){
+    evt.preventDefault();
+    
+    const userInput = $input.val()
+
+    //Check to make sure that there is something that was inputted before the Find Film button was clicked
+    if(!userInput) return; //get outta here
+
+    function charName(matchInput) {
+        return matchInput.name === userInput;
+      }
+      
+      selectedName = newArray.find(charName).name;  
+      selectedSpecies = newArray.find(charName).species;
+      selectedAncestry = newArray.find(charName).ancestry; 
+      selectedWizard = newArray.find(charName).wizard; 
+      selectedPatronus = newArray.find(charName).patronus;
+      selectedHouse = newArray.find(charName).house;
+      selectedImage = newArray.find(charName).image;
+
+      render();
+    
+    $input.val("");
+}
+
+
+  function render(){
+    $('main').html(`
+        <h3>Name: ${selectedName}</h3>
+        <p>Species: ${selectedSpecies}</p>
+        <p>Ancestry: ${selectedAncestry}</p>
+        <p>Wizard: ${selectedWizard}</p>
+        <p>Patronus: ${selectedPatronus}</p>
+        <p>House: ${selectedHouse}</p>          
+        <img src="${selectedImage}" alt="${selectedName}">
+`)}
